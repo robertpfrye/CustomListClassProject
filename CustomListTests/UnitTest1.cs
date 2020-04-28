99280e89814ee06e5f8f6d855a41cac638b347da
robertpfrye <robert.p.frye@gmail.com>
2020-04-28 16:18:16 -0700

    add and remove test methods for capacity 4 or less along with  CustomList stuff

--- CustomListTests/UnitTest1.cs
+++ CustomListTests/UnitTest1.cs
@@ -8,7 +8,7 @@ namespace CustomListTests
     public class UnitTest1
     {
         [TestMethod]
-        public void Add_AddingOneValueToEmptyCustomList_AddedValueGoesToIndexZero()
+        public void AddTest1_AddOneValue_AddedValueGoesToIndexZero()
         {
             // arrange
             CustomList<int> testList = new CustomList<int>();
@@ -25,7 +25,27 @@ namespace CustomListTests
         }
 
         [TestMethod]
-        public void Add_AddingOneValueToEmptyCustomList_CountOfCustomListIncrements()
+        public void AddTest2_AddThreeValuesToEmptyCustomList_WhatValueGoesToIndexTwo()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd1 = 10;
+            int itemToAdd2 = 20;
+            int itemToAdd3 = 30;
+            int expected = 30;
+            int actual;
+            // act
+            testList.Add(itemToAdd1);
+            testList.Add(itemToAdd2);
+            testList.Add(itemToAdd3);
+            actual = testList[2];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTest3_AddOneValue_CountOfList()
         {
             // arrange
             CustomList<int> testList = new CustomList<int>();
@@ -40,10 +60,150 @@ namespace CustomListTests
             // assert
             Assert.AreEqual(expected, actual);
         }
-        
+
+        [TestMethod]
+        public void AddTest4_AddingMultipleValues_CountOfList()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd1 = 10;
+            int itemToAdd2 = 20;
+            int itemToAdd3 = 30;
+            int expected = 3;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd1);
+            testList.Add(itemToAdd2);
+            testList.Add(itemToAdd3);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+
+        //Remove Tests
+
+        [TestMethod]
+        public void RemoveTest1_StartWithFourValuesRemoveOne_CheckingIndex1()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd1 = 10;
+            int itemToAdd2 = 20;
+            int itemToAdd3 = 30;
+            int itemToAdd4 = 40;
+            int expected = 30;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd1);
+            testList.Add(itemToAdd2);
+            testList.Add(itemToAdd3);
+            testList.Add(itemToAdd4);
+            testList.Remove(itemToAdd2);
+            actual = testList[1];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveTest2_StartWithFourValuesRemoveOne_CehckCount()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd1 = 10;
+            int itemToAdd2 = 20;
+            int itemToAdd3 = 30;
+            int itemToAdd4 = 40;
+            int expected = 30;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd1);
+            testList.Add(itemToAdd2);
+            testList.Add(itemToAdd3);
+            testList.Add(itemToAdd4);
+            testList.Remove(itemToAdd2);
+            actual = testList.Count;
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveTest3_StartWithFourValuesRemoveValue20_CheckIndex1()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd1 = 10;
+            int itemToAdd2 = 20;
+            int itemToAdd3 = 30;
+            int itemToAdd4 = 40;
+            int expected = 30;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd1);
+            testList.Add(itemToAdd2);
+            testList.Add(itemToAdd3);
+            testList.Add(itemToAdd4);
+            testList.Remove(20);
+            actual = testList[1];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveTest4_StartWithFourValuesRemoveOne_CheckingIndex1()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToAdd1 = 10;
+            int itemToAdd2 = 20;
+            int itemToAdd3 = 30;
+            int itemToAdd4 = 40;
+            int expected = 30;
+            int actual;
+
+            // act
+            testList.Add(itemToAdd1);
+            testList.Add(itemToAdd2);
+            testList.Add(itemToAdd3);
+            testList.Add(itemToAdd4);
+            testList.Remove(itemToAdd2);
+            actual = testList[1];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void RemoveTest5_StartWithOneVariableAndRemoveIt_WhatIsTheValueOfIndexZero()
+        {
+            // arrange
+            CustomList<int> testList = new CustomList<int>();
+            int itemToRemove1 = 10;
+            int expected = 0;
+            int actual;
+
+            // act
+            testList.Add(itemToRemove1);
+            testList.Remove(itemToRemove1);
+            actual = testList[0];
+
+            // assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
         // what happens if you add multiple things (or add to a CustomList that already has some values)?
-            // what happens to the last-added item?
-            // what happens to the Count?
+        // what happens to the last-added item?
+        // what happens to the Count?
 
         // what happens if you add more items than the initial Capacity of the CustomList?
 
--- Sandbox/CustomList.cs
+++ Sandbox/CustomList.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using DocumentFormat.OpenXml.Packaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sandbox
@@ -10,17 +12,70 @@ namespace Sandbox
     {
         // member variables (HAS A)
         private T[] items;
+        private int count;
+        private int capacity;
+
 
         // constructor (SPAWNER)
+
         public CustomList()
         {
+            count = 0;
             items = new T[4];
+            items = new T[capacity];
         }
 
+
         // member methods (CAN DO)
+
+        //Indexer
+        public T this[int index]
+        {
+            get
+            {
+                if (index < count && index >= 0)
+                {
+                    return items[index];
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            set
+            {
+                items[index] = value;
+            }
+        }
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+        }
+
+        //add
         public void Add(T item)
         {
+            items[count] = item;
+            count++;
+        }
 
+        //remove
+        public void Remove(T item)
+        {
+            items[count] = item;
+            count--;
         }
     }
 }
